2d2ccdf57f911bee2fe96cacea20431aca9fe0e0
BearB34R <andynguyendo@csu.fullerton.edu>
2024-03-05 18:53:00 -0800

    started

--- server.js
+++ server.js
@@ -2,19 +2,4 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/news", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/newsweb.html"));
-});
-
-app.set("view engine", "ejs");
-
-const userRouter = require("./routes/users");
-const newsRouter = require("./routes/news");
-
-app.use("/users", userRouter);
-app.use("/news", newsRouter);
-
 app.listen(process.env.PORT || 3000);
