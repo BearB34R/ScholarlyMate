ac71d9c033860bca3051809ce839a172860d0b40
BearB34R <andynguyendo@csu.fullerton.edu>
2024-04-14 17:47:45 -0700

    fixed order of bash build.sh/ didn't run correctly before

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ python3 -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
+#All AUTH
+pip install django-allauth
+
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
-#All AUTH
-pip install django-allauth
