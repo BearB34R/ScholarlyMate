877e8d114f6214b29b6859d233e0cec845415b01
BearB34R <andynguyendo@csu.fullerton.edu>
2024-04-18 16:53:16 -0700

    moved required files to requirements.txt

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ python3 -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
-#All AUTH
-pip install django-allauth
-
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
