aa5266f7cac3f598e257de6a9425c167d560eb25
Dominic Harrington <drharrington@csu.fullerton.edu>
2024-04-30 23:08:02 -0700

    Add login view test case
    Verifies if login view renders the correct template and returns successful HTTP response

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class LoginViewTestCase(TestCase):
+    def test_login_view(self):
+        # Get URL for login view
+        url = reverse('login')
+
+        # GET request to login view
+        response = self.client.get(url)
+
+        # Check if the view returns a status code of 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the correct template is used
+        self.assertTemplateUsed(response, 'accounts/login.html')
